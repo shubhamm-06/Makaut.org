4b6e977c4e23b9a2b214fad00a08da395b6e0f09
Shubham Kumar Singh <shubhamkrsingh06@gmail.com>
2023-07-29 14:10:04 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-//Get the button
+// Scroll to top button
 let mybutton = document.getElementById("btn-back-to-top");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
-
 function scrollFunction() {
   if (
     document.body.scrollTop > 20 ||
@@ -18,30 +16,24 @@ function scrollFunction() {
 }
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
-
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-
-
-
-
-
+// Search Bar
 const searchInput = document.getElementById("searchInput");
-        searchInput.addEventListener("input", () => {
-            const query = searchInput.value.toLowerCase();
-            const questions = document.getElementsByClassName("question");
+searchInput.addEventListener("input", () => {
+  const query = searchInput.value.toLowerCase();
+  const questions = document.getElementsByClassName("question");
 
-            Array.from(questions).forEach(question => {
-                const questionHeader = question.getElementsByTagName("p")[0];
-                const questionText = questionHeader.textContent.toLowerCase();
+  Array.from(questions).forEach(question => {
+    const questionHeader = question.getElementsByTagName("p")[0];
+    const questionText = questionHeader.textContent.toLowerCase();
 
-                if (questionText.includes(query)) {
-                    question.style.display = "block";
-                } else {
-                    question.style.display = "none";
-                }
-            });
-        });
+    if (questionText.includes(query)) {
+      question.style.display = "block";
+    } else {
+      question.style.display = "none";
+    }
+  });
+});
