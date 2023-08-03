54a642566b69303b578f3e80eea8a7ba64a8055a
Shubham Kumar Singh <shubhamkrsingh06@gmail.com>
2023-08-04 00:46:00 +0530

    udate

--- dynamic.js
+++ dynamic.js
@@ -1,34 +1,74 @@
 //  Content for abt page within notice
 const dynamicAbt = document.getElementById("abt");
-dynamicAbt.innerHTML = "At makaut.org, our overarching mission is to revolutionize the academic journey of MAKAUT (Maulana Abul Kalam Azad University of Technology) students and empower them to achieve unparalleled success. Our vision is to become the ultimate, all-encompassing resource hub, setting new standards in facilitating education and fostering a thriving learning community."
-// Content for holiday article
-const dynamicHoliday = document.getElementById("holiday");
-  dynamicHoliday.innerHTML = `
-  <h2 class="text-primary-emphasis fw-bold">MAKAUT 2023 HOLIDAY LIST</h2>
-  <a href="https://makautwb.ac.in/big_files/Holiday%20list%202023.pdf" class="btn btn-primary my-2" target="_blank" download>
-    <i class="fas fa-download"></i> Download PDF
-  </a>
-  <p>As the year 2023 unfolds, MAKAUT University gears up to welcome a plethora of holidays that promises both celebration and learning. MAKAUT University understands the significance of holidays not only in terms of relaxation and rejuvenation but also as an opportunity to embrace cultural diversity. For the year 2023, the university has thoughtfully planned a schedule that includes all national and religious holidays, such as Chhath Puja, Christmas, and Durga Puja. Students, faculty, and staff can look forward to ample time off to spend with their loved ones, recharge their minds, and pursue their interests. In this article, we will explore the exciting and enriching prospects that MAKAUT University's holiday calendar offers, making it a memorable year for everyone involved.</p>
+if (dynamicAbt) {
+    dynamicAbt.innerHTML = "At makaut.org, our overarching mission is to revolutionize the academic journey of MAKAUT (Maulana Abul Kalam Azad University of Technology) students and empower them to achieve unparalleled success. Our vision is to become the ultimate, all-encompassing resource hub, setting new standards in facilitating education and fostering a thriving learning community."
+} 
 
-  <h4 class="text-primary-emphasis">An Abundance of Holidays</h4>
 
-  <p>The holiday season at MAKAUT University for the year 2023 is nothing short of generous. The university has meticulously crafted the academic calendar to incorporate all the major national holidays, including Republic Day, Independence Day, and Gandhi Jayanti, allowing the university community to rejoice in the spirit of patriotism and unity. But that's not all; MAKAUT University also takes pride in recognizing and respecting the diversity of its students and staff, ensuring that religious holidays like Chhath Puja, Christmas, Eid, and Durga Puja are observed and celebrated with equal enthusiasm.</p>
 
-  <h4 class="text-primary-emphasis">Dive into Cultural Festivities</h4>
-
-  <p>One of the highlights of the MAKAUT University holiday 2023 calendar is the celebration of religious festivals. During Chhath Puja, the campus will come alive with vibrant colors, melodies of folk songs, and the harmonious blending of tradition and devotion. Students from different regions and backgrounds will come together, fostering cultural exchange and a sense of belonging.</p>
-
-  <p>As December approaches, the university will embrace the festive cheer of Christmas. The campus will be adorned with twinkling lights, Christmas trees, and warm smiles. Events like carol singing, Secret Santa exchanges, and cultural performances will add a touch of merriment to the holiday season.</p>
-
-  <p>Durga Puja, one of the most significant festivals in West Bengal, will be celebrated with grandeur and fervor at MAKAUT University. The campus will resonate with the beats of dhaak (drums) and the aroma of delicious traditional delicacies. Students will take pride in showcasing their artistic talents during cultural competitions, enhancing the festive spirit.</p>
-
-  <h4 class="text-primary-emphasis">Balancing Leisure and Learning</h4>
-
-  <p>While holidays are meant for relaxation, MAKAUT University also encourages its community to utilize this time for personal and professional growth. Various workshops, seminars, and webinars will be organized during the holidays, offering participants opportunities to enhance their skills, expand their knowledge, and prepare for the upcoming semesters.</p>
-
-  <h4 class="text-primary-emphasis">Conclusion</h4>
-
-  <p>The holiday season at MAKAUT University for the year 2023 promises to be an unforgettable experience for students, faculty, and staff. With a diverse array of holidays, including national and religious observances, the campus will be immersed in the spirit of unity and celebration. As the festivities of Chhath Puja, Christmas, Eid, and Durga Puja unfold, cultural exchange will be at its peak, promoting a deeper understanding and appreciation of different traditions. Additionally, the university encourages the pursuit of knowledge and growth during this time, providing ample opportunities for personal and professional development. So, as you embrace the MAKAUT University holiday 2023, remember to revel in the festivities, cherish the moments of togetherness, and embark on a journey of learning and enrichment.</p>
-
-  <p><em>Note: For a detailed list of holidays for the year 2023, you can refer to the PDF provided by MAKAUT University on their official website.</em></p>
+const container = document.getElementById("dynamicCardContainer");
+if(container){
+function createCard(title, date, description, imageUrl, linkUrl) {
+  const cardContainer = document.createElement("div");
+  cardContainer.innerHTML = `
+<div class="card flex-md-row mb-4 box-shadow h-md-250">
+  <div class="card-body d-flex flex-column align-items-start">
+    <h3 class="mb-0">
+      <a class="text-primary-emphasis" href="${linkUrl}">${title}</a>
+    </h3>
+    <div class="mb-1 text-muted">${date}</div>
+    <p class="card-text mb-auto">${description}</p>
+    <a href="${linkUrl}">Continue reading</a>
+  </div>
+  <img class="card-img-right flex-auto d-none d-md-block" src="${imageUrl}" width="200" alt="Card image cap">
+</div>
 `;
+
+  // Append the dynamically generated card to the container on the page
+  container.appendChild(cardContainer);
+}
+// Example usage:
+createCard(
+  "MAKAUT 2023 HOLIDAY LIST",
+  "August 15",
+  "This iAs the year 2023 unfolds, MAKAUT University gears up to welcome a plethora of holidays that promises both celebration and learning.s a wider card with supporting text below as a natural lead-in to additional content.",
+  "/Img/writing.jpg",
+  "/Notice/Notice/holiday.html"
+);
+createCard(
+  "Academic Calender",
+  "August 15",
+  "The long-awaited moment has arrived as MAKAUT has recently published its comprehensive Academic Calendar for the academic year 2023-24. ",
+  "/Img/writing.jpg",
+  "/Notice/Notice/calender.html"
+);
+}
+
+
+(function() {
+  // Function to dynamically change content
+  function changeNewsContent() {
+      // Sample content to be sent dynamically
+      const dynamicContent = {
+          title: "MAKAUT AFFILIATED COLLEGES",
+          leadText: "Ranking of best colleges under makaut lets see",
+          linkUrl: "https://makautwb.ac.in/big_files/College%20list-2022-23-as%20on%2031-01-2023.pdf",
+          linkText: "Read More",
+      };
+
+      // Get the <div> element by its ID
+      const newsContainer = document.getElementById("news-container");
+
+      // Update the content of the elements inside the <div>
+      if (newsContainer) {
+          newsContainer.querySelector("h1").innerText = dynamicContent.title;
+          newsContainer.querySelector(".lead").innerText = dynamicContent.leadText;
+          const linkElement = newsContainer.querySelector(".lead a");
+          linkElement.href = dynamicContent.linkUrl;
+          linkElement.innerText = dynamicContent.linkText;
+      }
+  }
+
+  // Call the function to change the content
+  changeNewsContent();
+})();
